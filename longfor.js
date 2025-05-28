a1888a3c766d8159484e2ebf23026e2b39ac9716
XQ <xiaoqian@tha-tha-4.local>
2025-05-28 22:39:24 +0800

    11

--- longfor.js
+++ longfor.js
@@ -4,7 +4,8 @@
 
 [rewrite_local]
 
-^https:\/\/.*\.longfor\.com\/management\/app\/api\/uHome\/ext\/checkOweFee.*$ url script-response-body https://raw.githubusercontent.com/stupid-pao/Rewrite/refs/heads/master/longfor.js
+^https:\/\/.*\/.longfor\/.com\/supera\/C4\/v1_13_0\/entranceGuard\/checkOweFee.*$ url script-response-body https://raw.githubusercontent.com/stupid-pao/Rewrite/refs/heads/master/longfor.js
+
 
 [mitm]
 hostname = *.longfor.com
@@ -20,8 +21,10 @@ try {
     let obj = JSON.parse(body);
     console.log(obj)
     final = {
-        "status": "ok",
-        "code": 0,
+        "code": "200",
+        "message": "ok",
+        "extraMessage": "",
+        "extraCode": "",
         "data": {
             "closeSecond": 0,
             "paymentLink": null,
@@ -34,9 +37,6 @@ try {
             // "tipContent": "您有物业费待缴纳，请您尽快查看并支付。如已缴纳请忽略。",
             // "oweFee": true
         },
-        "traceId": obj.traceId,
-        "msg": "ok",
-        "timestamp": obj.timestamp 
     }
 
     body = JSON.stringify(final);
@@ -65,22 +65,25 @@ try {
   "timestamp": 1744780948153
 }
 
-{
-  "status": "ok",
-  "code": 0,
-  "data": {
-    "closeSecond": 5,
-    "paymentLink": "https:\/\/c4.longfor.com\/uxj-h5\/property?webView=maia&channelCode=entrance",
-    "tipTitle": "温馨提示",
-    "tipContent": "您有物业费待缴纳，请您尽快查看并支付。如已缴纳请忽略。",
-    "oweFee": true
-  },
-  "traceId": "10efaf8c4b184aed8e75c9d1aab325a0",
-  "msg": "ok",
-  "timestamp": 1744774631672
-}
+
 
 {"status":"ok","traceId":"d3fb3a3420094642b36ecd77ed924b1f","msg":"ok","code":0,"data":{"tipTitle":null,"tipContent":null,"oweFee":false,"closeSecond":0,"paymentLink":null},"timestamp":1744782036120}{"status":"ok","traceId":"d3fb3a3420094642b36ecd77ed924b1f","msg":"ok","code":0,"data":{"tipTitle":null,"tipContent":null,"oweFee":false,"closeSecond":0,"paymentLink":null},"timestamp":1744782036120}{"status":"ok","traceId":"d3fb3a3420094642b36ecd77ed924b1f","msg":"ok","code":0,"data":{"tipTitle":null,"tipContent":null,"oweFee":false,"closeSecond":0,"paymentLink":null},"timestamp":1744782036120}
+
+
+
+{
+  "code": "200",
+  "message": "ok",
+  "extraMessage": "",
+  "extraCode": "",
+  "data": {
+    "closeSecond": 5,
+    "paymentLink": "https:\/\/supera-c4.wan-prod.longfor.com\/property\/home?orderSource=SUPER_APP&channelCode=caappservice",
+    "tipTitle": "温馨提示",
+    "oweFee": true,
+    "tipContent": "您有物业费待缴纳，请您尽快查看并支付。如已缴纳请忽略。"
+  }
+}
   */
 
 
